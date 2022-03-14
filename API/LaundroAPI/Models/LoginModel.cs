bd4ef903fd64e415c6ec5cea0b76bf49b9823da7
anissfadel123 <anissfadel123@yahoo.com>
2022-03-14 18:40:47 -0400

    Add Authentication and WebAPI Application
    Added a controller for user authentication
    and configured jwt bearer token.

---
+++ API/LaundroAPI/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundroAPI.Models
+{
+    public class LoginModel
+    {
+
+        [Required(ErrorMessage = "Username or Email is required")]
+        public string UsernameOrEmail { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
