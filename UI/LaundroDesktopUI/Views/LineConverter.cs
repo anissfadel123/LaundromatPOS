05eb6013a7afaa0697c27f630d51e1bdc7485233
anissfadel123 <anissfadel123@yahoo.com>
2022-03-25 20:01:31 -0400

    Create NewSale Page UI
    Developed most of the UI for the New Sale page,
    added poppins font as default font for entire app, and
    inserted a few image assets.

---
+++ UI/LaundroDesktopUI/Views/LineConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LaundroDesktopUI.Views
+{
+    public class LineConverter : IValueConverter
+    {
+        public double Decrease { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double a = 0;
+            if (value != null)
+            {
+                try
+                {
+                    a = System.Convert.ToDouble(value);
+                }
+                catch
+                {
+                    a = 0;
+                }
+            }
+            return a - Decrease;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double a = 0;
+            if (value != null)
+            {
+                try
+                {
+                    a = System.Convert.ToDouble(value);
+                }
+                catch
+                {
+                    a = 0;
+                }
+            }
+            return a + Decrease;
+        }
+    
+    }
+}
