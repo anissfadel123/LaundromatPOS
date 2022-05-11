d3e86491a950b7fe1b231a9445ed001ec15d2655
anissfadel123 <anissfadel123@yahoo.com>
2022-05-11 18:55:20 -0400

    Clean Page Web UI
    Completed clean page aesthetic.

--- UI/laundromat-web-ui/src/app/new-sale/products/products.component.ts
+++ UI/laundromat-web-ui/src/app/new-sale/products/products.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+  }
+
 
 }
